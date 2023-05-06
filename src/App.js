abcff25a80883e1e64eb8e50fa3a699c672bf536
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-06 16:07:35 +0530

    v1 Delta 1.4
    Created the Counter Component and rendered in the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       App
+      <Counter />
     </div>
   );
 }
