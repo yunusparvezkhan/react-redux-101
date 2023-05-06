daf707d5343a7fdf2b9c6257cc1aa296ef6e4b2d
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-06 16:17:11 +0530

    v1 Delta 1.5
    used Redux State and Actions in React Component Counter.js

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
 
 const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
     return (
-        <div>Counter</div>
+        <div>
+            <div>
+                <button onClick={() => dispatch(increment())}>Increment</button>
+                <span>{count}</span>
+                <button onClick={() => dispatch(decrement())}>Decrement</button>
+            </div>
+        </div>
     )
 }
 
